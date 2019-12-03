94172fa53446132749452a01d8580a0118f70a46
xoyc00 <32788659+xoyc00@users.noreply.github.com>
2019-12-03 21:27:04 +1100

    Update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom os_32.iso -m 512M
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom os_32.iso -m 512M -enable-kvm
